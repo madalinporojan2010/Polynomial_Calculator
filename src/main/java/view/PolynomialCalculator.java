ddaf438dc1301ee6103bab397433c261e007c4ba
madalinporojan <porojan.co.madalin@student.utcluj.ro>
2022-02-16 10:19:59 +0200

    mvc

---
+++ src/main/java/view/PolynomialCalculator.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.*;
+
+public class PolynomialCalculator {
+    private JButton multiplicationButton;
+    private JButton subtractionButton;
+    private JButton additionButton;
+    private JButton divisionButton;
+    private JTextField textField1;
+    private JTextField textField2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
