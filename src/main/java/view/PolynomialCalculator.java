319aa9c93c5e4e74fe6eb0f8dcc2535d946b58c7
madalinporojan <porojan.co.madalin@student.utcluj.ro>
2022-02-20 13:27:23 +0200

    gui

--- src/main/java/view/PolynomialCalculator.java
+++ src/main/java/view/PolynomialCalculator.java
@@ -12,8 +12,16 @@ public class PolynomialCalculator {
     private JTextField textField3;
     private JButton derrivationButton;
     private JButton integrationButton;
+    private JPanel panel1;
+    private JFrame frame;
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+    public PolynomialCalculator(){
+        frame = new JFrame("UTCN aplicatie");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setContentPane(panel1);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setResizable(false);
     }
 }
